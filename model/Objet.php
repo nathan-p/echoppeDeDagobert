c53cabe23310290fd1b4bc592f2c20c33afb97a7
AlixDel <alixDel396@gmail.com>
2015-01-30 11:51:13 +0100

    constructeur et corrections dans Objet.php

--- model/Objet.php
+++ model/Objet.php
@@ -11,41 +11,88 @@ class Objet {
 	private $urlImage;
 	private $SousCategorie_idCategorie;
 	
-	public __constructor() {
+	public function __constructor(array $donnees) {
+
+		foreach ($donnees as $key => $value)
+		{
+			// On récupère le nom du setter correspondant à l'attribut.
+			$method = 'set'.ucfirst($key);
+
+			// Si le setter correspondant existe.
+			if (method_exists($this, $method))
+			{
+				// On appelle le setter.
+				$this->$method($value);
+			}
+		}
 	}
+
 	
 	public function setIdObjet($id) {
 		$id = (int) $id;
 		
 		if ($id > 0) {
-			this->$idObjet = $id;
+			$this->$idObjet = $id;
 		}
 	}
+
+	/**
+	 * @param mixed $nom
+	 */
+	public function setNom($nom)
+	{
+		$this->nom = $nom;
+	}
+
+	/**
+	 * @param mixed $promotion
+	 */
+	public function setPromotion($promotion)
+	{
+		$this->promotion = $promotion;
+	}
+
+	/**
+	 * @param mixed $urlImage
+	 */
+	public function setUrlImage($urlImage)
+	{
+		$this->urlImage = $urlImage;
+	}
+
+	/**
+	 * @param mixed $SousCategorie_idCategorie
+	 */
+	public function setSousCategorieIdCategorie($SousCategorie_idCategorie)
+	{
+		$this->SousCategorie_idCategorie = $SousCategorie_idCategorie;
+	}
+
 	
 	public function setDescription($desc) {
 		$desc = (String) $desc;
 		
-		this->$description = $desc;
+		$this->$description = $desc;
 	}
 	
-	public function setIdObjet($s) {
+	public function setStock($s) {
 		$s = (int) $s;
 		
 		if ($s > 0) {
-			this->$stock = $id;
+			$this->$stock = $s;
 		}
 	}
 	
-	public function setPrix($id) {
-		$id = (int) $id;
+	public function setPrix($p) {
+		$p = (float) $p;
 		
-		if ($id > 0) {
-			this->$idObjet = $id;
+		if ($p > 0) {
+			$this->$prix = $p;
 		}
 	}
 	
 	
-	http://openclassrooms.com/courses/programmez-en-oriente-objet-en-php/manipulation-de-donnees-stockees
+
 }
 	
 	
