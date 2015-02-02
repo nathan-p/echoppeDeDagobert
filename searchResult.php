fc92d35da11c95fcab342e53c8faa59508ff3290
nathan-p <nathan.prior.31@gmail.com>
2015-02-02 16:30:05 +0100

    search

---
+++ searchResult.php
@@ -0,0 +1,72 @@
+<?php include("header.php"); ?>
+
+<div class="container">
+    <div class="row">
+    	
+        <h1>Resultat de votre recherche</h1>
+        <br><br>
+        <div class="list-group">
+          <a class="list-group-item">
+                <div class="media col-md-3">
+                    <figure class="pull-left">
+                        <img class="media-object img-rounded img-responsive"  src="http://placehold.it/350x250" alt="placehold.it/350x250" >
+                    </figure>
+                </div>
+                <div class="col-md-6">
+                    <h4 class="list-group-item-heading"> List group heading </h4>
+                    <p class="list-group-item-text"> Qui diam libris ei, vidisse incorrupte at mel. His euismod salutandi dissentiunt eu. Habeo offendit ea mea. Nostro blandit sea ea, viris timeam molestiae an has. At nisl platonem eum. 
+                        Vel et nonumy gubergren, ad has tota facilis probatus. Ea legere legimus tibique cum, sale tantas vim ea, eu vivendo expetendis vim. Voluptua vituperatoribus et mel, ius no elitr deserunt mediocrem. Mea facilisi torquatos ad.
+                    </p>
+                </div>
+                <div class="col-md-3 text-center">
+                    <h2> 29€ <small> 90 </small></h2>
+                    <button type="button" onclick="self.location.href='./index.php'" class="btn btn-default btn-lg btn-block"> 
+						<span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> 
+                    	Ajouter au panier 
+                    </button>
+                    <div class="stars">
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                    </div>
+                    <p> Moyenne 4.5 <small> / </small> 5 </p>
+                </div>
+          </a>
+                    <a class="list-group-item">
+                <div class="media col-md-3">
+                    <figure class="pull-left">
+                        <img class="media-object img-rounded img-responsive"  src="http://placehold.it/350x250" alt="placehold.it/350x250" >
+                    </figure>
+                </div>
+                <div class="col-md-6">
+                    <h4 class="list-group-item-heading"> List group heading </h4>
+                    <p class="list-group-item-text"> Qui diam libris ei, vidisse incorrupte at mel. His euismod salutandi dissentiunt eu. Habeo offendit ea mea. Nostro blandit sea ea, viris timeam molestiae an has. At nisl platonem eum. 
+                        Vel et nonumy gubergren, ad has tota facilis probatus. Ea legere legimus tibique cum, sale tantas vim ea, eu vivendo expetendis vim. Voluptua vituperatoribus et mel, ius no elitr deserunt mediocrem. Mea facilisi torquatos ad.
+                    </p>
+                </div>
+                <div class="col-md-3 text-center">
+                    <h2> 64€ <small> 90 </small></h2>
+                    <button type="button" onclick="self.location.href='./index.php'" class="btn btn-default btn-lg btn-block"> 
+						<span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> 
+                    	Ajouter au panier 
+                    </button>
+                    <div class="stars">
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                    </div>
+                    <p> Moyenne 2.9 <small> / </small> 5 </p>
+                </div>
+          </a>
+        </div>
+
+	</div>
+</div>
+</div>
+
+
+<?php include("footer.php"); ?>
